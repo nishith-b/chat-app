06ca2388fa1e0cc9b0166ea54e301fa51d8ee06b
nishith <nishith.one@gmail.com>
2025-07-10 07:38:33 +0530

    Added create new chat and get all chat api

--- BACKEND/chat-service/src/controllers/chat.ts
+++ BACKEND/chat-service/src/controllers/chat.ts
@@ -1,6 +1,7 @@
 import TryCatch from "../config/try-catch";
 import { AuthenticatedRequest } from "../middlewares/isAuth.js";
 import { Chat } from "../models/chat";
+import { Messages } from "../models/Messages";
 
 export const createNewChat = TryCatch(
   async (req: AuthenticatedRequest, res) => {
@@ -49,8 +50,47 @@ export const getAllChats = TryCatch(async (req: AuthenticatedRequest, res) => {
   const chatWithUserData = await Promise.all(
     chats.map(async (chat) => {
       const otherUserId = chat.users.find((id) => id !== userId);
+      const unseenCount = await Messages.countDocuments({
+        chatId: chat._id,
+        sender: { $ne: userId },
+        seen: false,
+      });
 
-      
+      try {
+        //This is why this route is made free of auth in user service
+        const { data } = await axios.get(
+          `${process.env.USER_SERVICE}/api/v1/user/${otherUserId}`
+        );
+
+        return {
+          user: data,
+          chat: {
+            ...chat.toObject(),
+            latestMessage: chat.latestMessage || null,
+            unseenCount,
+          },
+        };
+      } catch (error) {
+        console.log(error);
+        return {
+          user: { _id: otherUserId, name: "Unknown User" },
+          chat: {
+            ...chat.toObject(),
+            latestMessage: chat.latestMessage || null,
+            unseenCount,
+          },
+        };
+      }
     })
   );
+
+  res.json({
+    chats: chatWithUserData,
+  });
+});
+
+export const sendMessage = TryCatch(async (req: AuthenticatedRequest, res) => {
+  const senderId = req.user?._id;
+  const { chatId, text } = req.body;
+  const imageFile = req;
 });
--- BACKEND/chat-service/src/routes/chat-routes.ts
+++ BACKEND/chat-service/src/routes/chat-routes.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { isAuth } from "../middlewares/isAuth";
-import { createNewChat } from "../controllers/chat";
+import { createNewChat, getAllChats } from "../controllers/chat";
 
 const router = express.Router();
 
 router.post("/chat/new", isAuth, createNewChat);
+router.get("/chat/all", isAuth, getAllChats);
 
 export default router;
